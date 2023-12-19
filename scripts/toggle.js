37c9e4f6a9168ad5c2d06e37f7af62fb3e429f3c
jeebuscrossaint <thediamond270@gmail.com>
2023-12-19 14:30:45 -0500

    made a todo list

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,23 +1,17 @@
-// Get the theme toggle input and icon
 var themeToggle = document.getElementById('theme-toggle');
 var themeIcon = document.getElementById('theme-icon');
 
-// Set the initial theme based on local storage or default to 'dark'
 var currentTheme = localStorage.getItem('theme') || 'dark';
 document.documentElement.setAttribute('data-theme', currentTheme);
 
-// Update the icon based on the current theme
 themeIcon.className = currentTheme === 'dark' ? 'fas fa-moon slider round' : 'far fa-sun slider round';
 
-// Add an event listener to the input to toggle the theme
 themeToggle.addEventListener('change', function() {
     var currentTheme = document.documentElement.getAttribute('data-theme');
     var newTheme = currentTheme === 'dark' ? 'light' : 'dark';
 
-    // Set the new theme
     document.documentElement.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
 
-    // Update the icon
     themeIcon.className = newTheme === 'dark' ? 'fas fa-moon slider round' : 'far fa-sun slider round';
 });
---
+++ todo.md
@@ -0,0 +1,7 @@
+# TODO List
+
+1. make the dark mode light mode switcher actually good
+2. add actual content
+3. write funny script
+4. make different html pages n stuff to make it nicer
+5. hopefully thats it
